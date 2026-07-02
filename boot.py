79eb0c21d31d8cab586d5197942b02f44e325285
Tom Hoffman <tom.hoffman@gmail.com>
2026-07-01 22:47:54 -0400

    Refactor USB MIDI setup in boot.py
    Removed redundant USB MIDI names setup and improved code structure.

--- boot.py
+++ boot.py
@@ -6,24 +6,14 @@
 # sets the name of the Circuit Playground as it appears to a PC
 # from https://github.com/todbot/circuitpython-tricks#usb
 
-<<<<<<< HEAD
-# Also essentially spamming all the other USB MIDI related names
-# to try to ensure your DAW and OS will be able to differentiate
-# between different modules.
-=======
 # Note that all the USB indicators might not show up without unplugging the board
 # and (on Ubunutu) flushing the USB cache:
 # `sudo systemctl restart alsa-state``
->>>>>>> 7d4acad (Performance improvements and better tap w/help from Gemini.)
 
 import usb-midi
 import supervisor
 import config
 import storage
-import usb_midi
-import supervisor
-
-
 
 supervisor.set_usb_identification(
 	manufacturer="PERFEC Systems",
@@ -32,14 +22,7 @@ supervisor.set_usb_identification(
 
 storage.remount("/", readonly=False)
 m = storage.getmount("/")
-n = config.USB_NAME
-m.label = n
-usb_midi.set_names(streaming_interface_name = n + "-STR",
-				   audio_control_interface_name =n  + "-AUD",
-				   in_jack_name = n + "-IN",
-				   out_jack_name = n + "-OUT")
-supervisor.set_usb_identification(manufacturer="PERFEC", 
-                                  product=n)
+m.label = config.USB_NAME
 storage.remount("/", readonly=True)
 
 usb_midi.set_names(streaming_interface_name = "CLOCK OUT")
